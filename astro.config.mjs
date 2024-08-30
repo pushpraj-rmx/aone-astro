dbbacc23576420ad51487c0fd16e2f6a4a50f057
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-08-30 10:42:05 +0530

    sitemap, canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 import viteConfig from './vite.config.js';
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: viteConfig,
-  integrations: [react()]
+  integrations: [react(), sitemap()],
+  site: 'https://theaoneglobal.in',
+  output: 'static'
 });
